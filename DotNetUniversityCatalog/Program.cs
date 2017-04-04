b927447a1b2f59b199354e36fb876e862edc10f9
devalexer <alliemckay.dev@gmail.com>
2017-04-04 17:54:22 -0400

    Add project files.

---
+++ DotNetUniversityCatalog/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetUniversityCatalog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string connectionString = @"Server=localhost\SQLEXPRESS;Database=DotNetUniversityCatalog;Trusted_Connection=True;";
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var sqlCommand = new SqlCommand(@"SELECT Course.CourseTitle, Instructor.InstName
+                                                FROM Course
+                                                JOIN Instructor ON Course.InstName = Instructor.Id", 
+                                                connection);
+                connection.Open();
+                var reader = sqlCommand.ExecuteReader();
+                while (reader.Read())
+                {
+                    Console.WriteLine(reader["CourseTitle"] + "--" + reader["InstName"]);
+                }
+
+                connection.Close();
+            }
+
+        }
+    }
+}
